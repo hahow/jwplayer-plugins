2e7fded73cfb4140636c24a16e6ed9a28b4607d7
Amo Wu <amo260@gmail.com>
2022-09-05 10:09:06 +0800

    Revert "build: npx lerna create jwplayer-plugin-full-viewport --es-module"
    This reverts commit d84c30312724a619631073da60684d73d25064a7.

--- packages/jwplayer-plugin-full-viewport/__tests__/jwplayer-plugin-full-viewport.test.js
+++
@@ -1,5 +0,0 @@
-import jwplayerPluginFullViewport from '../src/jwplayer-plugin-full-viewport.js';
-import { strict as assert } from 'assert';
-
-assert.strictEqual(jwplayerPluginFullViewport(), 'Hello from jwplayerPluginFullViewport');
-console.info("jwplayerPluginFullViewport tests passed");
