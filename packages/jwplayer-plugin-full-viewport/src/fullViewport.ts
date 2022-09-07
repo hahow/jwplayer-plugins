1dd68cef3937fe68ab983c59403bed3727d0bcc3
Amo Wu <amo260@gmail.com>
2022-09-07 14:11:05 +0800

    refactor: 新增 empty package "@hahow/jwplayer-plugin-full-viewport"（劇院模式）

---
+++ packages/jwplayer-plugin-full-viewport/src/fullViewport.ts
@@ -0,0 +1,18 @@
+import initPlugin from "./initPlugin";
+
+// JW Player custom plugin
+// https://developer.jwplayer.com/jwplayer/docs/jw8-add-a-custom-or-third-party-plugin
+// https://github.com/jwplayer/jwplayer/blob/master/test/files/plugin1.js
+// https://github.com/jwplayer/jwplayer-sdks/blob/master/jw6-plugin-sdk/docs/plugins-js.md
+// https://github.com/jwplayer/jwplayer/issues/3444#issuecomment-508751780
+
+// Closure to prevent this code from conflicting with other scripts
+(function (global) {
+  const registerPlugin =
+    global.jwplayerPluginJsonp ||
+    (global.jwplayer && global.jwplayer().registerPlugin) ||
+    function () {};
+
+  // This line registers above code as a 8.0 compatible plugin called "fullViewport".
+  registerPlugin("fullViewport", "8.0", initPlugin);
+})(window);
